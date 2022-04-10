9be4b26ba1da55e5d359df0694470d7f4b090500
Liam Fenneman <liamfennemanbusiness@gmail.com>
2022-04-11 00:12:33 +1200

    Implement parsing assignment

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,44 @@
 mod utils;
 
 use std::{error::Error, fs, io, process};
-pub use utils::{Args, Assignment, Class};
+pub use utils::{Args, Assignment};
 
 /// Entry point to the program
 pub fn run(args: Args) -> Result<(), Box<dyn Error>> {
     // read the file from the args
     let contents = fs::read_to_string(args.filename())?;
-    println!("{} contents:", args.filename());
+    // println!("{} contents:", args.filename());
+    // for line in contents.lines() {
+    //     println!("{}", line)
+    // }
+
     for line in contents.lines() {
-        println!("{}", line)
+        let assign: Assignment = line.parse().unwrap_or_else(|err| {
+            eprintln!("{}", err);
+            process::exit(1);
+        });
+        println!("{}", assign);
     }
-
-    // get user input
-    let input = get_input("Write something").unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        process::exit(1);
-    });
-    println!("You wrote: {}", input);
-
-    // create a class
-    let c = Class::new("NWEN303").unwrap_or_else(|err| {
-        eprintln!("{}", err);
-        process::exit(1);
-    });
-    println!("New class: {}", c);
 
     // create an assignment
-    let mut a = Assignment::new("Test", 50.0, c);
-    println!("New assignment: {}", a);
-    if let Err(e) = a.set_mark(88.0) {
-        eprintln!("Assignment error: {}", e);
+    // let mut a = Assignment::new("Test", 50.0, "SOME101").unwrap();
+    // println!("New assignment: {}", a);
+    // if let Err(e) = a.set_mark(88.0) {
+    //     eprintln!("Assignment error: {}", e);
+    //     process::exit(1);
+    // }
+    // println!("With mark: {}", a);
+
+    // get user input
+    let input = get_input("Command or exit (q)").unwrap_or_else(|err| {
+        eprintln!("{}", err);
         process::exit(1);
+    });
+
+    // exit if the user types "q", "quit" or "exit"
+    if input == "q" || input == "quit" || input == "exit" {
+        process::exit(0);
     }
-    println!("With mark: {}", a);
 
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use std::{cmp, env, fmt};
+use std::{cmp, env, error::Error, fmt, str::FromStr};
 
 /// Wrapper for arguments passed to the program.
 pub struct Args {
@@ -25,67 +25,50 @@ impl Args {
     }
 }
 
-/// The class which an assignment can belong to.
-#[derive(Eq, PartialEq, Ord, PartialOrd, Debug)]
-pub struct Class {
-    pub code: String,
-}
-
-impl Class {
-    /// Create a new class.
-    ///
-    /// Code format: 4 capital letters + 3 digits
-    ///
-    /// # Examples
-    /// Valid code format:
-    /// ```
-    /// let class = tracker::Class::new("SOME101");
-    /// assert!(class.is_ok());
-    /// ```
-    /// Invalid code format:
-    /// ```
-    /// let class = tracker::Class::new("some random text");
-    /// assert!(class.is_err());
-    /// ```
-    pub fn new(code: &str) -> Result<Self, &'static str> {
-        let re = match regex::Regex::new(r"^[A-Z]{4}\d{3}$") {
-            Ok(re) => re,
-            Err(_) => return Err("Regex error"),
-        };
-
-        if !re.is_match(&code) {
-            return Err("Code doesn't follow format: XXXX### (e.g. EXPL101)");
-        }
-
-        Ok(Self {
-            code: code.to_string(),
-        })
-    }
-}
-
-impl fmt::Display for Class {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.code)
-    }
-}
-
-#[derive(PartialEq)]
+#[derive(PartialEq, Debug)]
 pub struct Assignment {
     name: String,
     mark: Option<f64>,
     value: f64,
-    class: Class,
+    class_code: String,
 }
 
 impl Assignment {
     /// Create a new Assignment.
-    pub fn new(name: &str, value: f64, class: Class) -> Self {
-        Self {
+    ///
+    /// # Conditions
+    /// - ```name``` length within range ```3..=20```
+    /// - ```value``` within range ```0..=100```
+    /// - ```class``` format: ```XXXX###``` (e.g. SOME101)
+    ///
+    /// # Examples
+    /// ```
+    /// let valid = tracker::Assignment::new("Test", 10.0, "SOME101");
+    /// assert!(valid.is_ok());
+    /// ```
+    pub fn new(name: &str, value: f64, class_code: &str) -> Result<Self, &'static str> {
+        if !(3..=20).contains(&name.len()) {
+            return Err("Name must have at least 1 char and at most 20 chars");
+        }
+
+        let re = regex::Regex::new(r"^[A-Z]{4}\d{3}$").unwrap_or_else(|err| {
+            panic!("Regex error: {}", err);
+        });
+
+        if !re.is_match(&class_code) {
+            return Err("Class code doesn't follow format: XXXX### (e.g. SOME101)");
+        }
+
+        if !(0.0..=100.0).contains(&value) {
+            return Err("Value of an assignment should be in range 0..=100");
+        }
+
+        Ok(Self {
             name: name.to_string(),
             mark: None,
             value,
-            class,
-        }
+            class_code: class_code.to_string(),
+        })
     }
 
     /// Get the name of the assignment.
@@ -105,8 +88,7 @@ impl Assignment {
     ///
     /// # Example
     /// ```
-    /// let class = tracker::Class::new("SOME101").unwrap();
-    /// let mut assign = tracker::Assignment::new("Test 1", 50.0, class);
+    /// let mut assign = tracker::Assignment::new("Test 1", 50.0, "SOME101").unwrap();
     /// assert!(assign.set_mark(80.0).is_ok());
     /// assert!(assign.set_mark(-80.0).is_err());
     /// assert!(assign.set_mark(200.0).is_err());
@@ -140,6 +122,38 @@ impl Assignment {
             None => None,
         }
     }
+
+    /// Get the class code for this assignment.
+    pub fn class_code(&self) -> &str {
+        &self.class_code
+    }
+
+    /// Get the Assignment in CSV format.
+    ///
+    /// # Examples
+    /// ```
+    /// let mut a = tracker::Assignment::new("Test 1", 25.0, "TEST123").unwrap();
+    /// assert_eq!("TEST123,Test 1,None,25.0", a.as_csv());
+    /// a.set_mark(99.9);
+    /// assert_eq!("TEST123,Test 1,99.9,25.0", a.as_csv());
+    /// ```
+    pub fn as_csv(&self) -> String {
+        match self.mark() {
+            Some(mark) => format!(
+                "{},{},{:.1},{:.1}",
+                self.class_code(),
+                self.name(),
+                mark,
+                self.value()
+            ),
+            None => format!(
+                "{},{},None,{:.1}",
+                self.class_code(),
+                self.name(),
+                self.value()
+            ),
+        }
+    }
 }
 
 impl fmt::Display for Assignment {
@@ -147,8 +161,8 @@ impl fmt::Display for Assignment {
         match self.mark() {
             Some(mark) => write!(
                 f,
-                "{}::{} [Mark: {} | Worth: {} | Pct: {:?}%]",
-                self.class,
+                "{} :: {} [Mark: {:.1} | Worth: {:.1} | Pct: {:.1}%]",
+                self.class_code,
                 self.name,
                 mark,
                 self.value,
@@ -156,8 +170,8 @@ impl fmt::Display for Assignment {
             ),
             None => write!(
                 f,
-                "{}::{} [No mark | Worth: {}]",
-                self.class, self.name, self.value
+                "{} :: {} [No mark | Worth: {:.1}]",
+                self.class_code, self.name, self.value
             ),
         }
     }
@@ -169,34 +183,196 @@ impl PartialOrd for Assignment {
     }
 }
 
+// Allow for parsing to Assignment from CSV
+impl FromStr for Assignment {
+    type Err = AssignmentParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split: Vec<&str> = s.trim().split(',').collect();
+
+        // parse the value into f64
+        let value: f64 = split[3].parse().unwrap();
+
+        // create an assignment, if err then return a parse error
+        let mut assignment = match Assignment::new(split[1], value, split[0]) {
+            Ok(m) => m,
+            Err(e) => return Err(AssignmentParseError::new(&format! {"{:?}", e})),
+        };
+
+        // if the split for mark is not "None" then try parse f64
+        if split[2] != "None" {
+            let mark: f64 = match split[2].parse() {
+                Ok(m) => m,
+                Err(e) => return Err(AssignmentParseError::new(&format!("{:?}", e))),
+            };
+            // try set the mark
+            if let Err(e) = assignment.set_mark(mark) {
+                return Err(AssignmentParseError::new(&format!("{:?}", e)));
+            }
+        }
+
+        Ok(assignment)
+    }
+}
+
+/// Error occurs when parsing a string into Assignment.
+pub struct AssignmentParseError {
+    msg: String,
+}
+
+impl AssignmentParseError {
+    pub fn new(msg: &str) -> Self {
+        Self {
+            msg: msg.to_string(),
+        }
+    }
+}
+
+impl Error for AssignmentParseError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        None
+    }
+}
+
+impl fmt::Display for AssignmentParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error parsing assignment: {}", self.msg)
+    }
+}
+
+impl fmt::Debug for AssignmentParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "AssignmentParseError {{ msg: {} }}", self.msg)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn new_class() {
-        let class = Class::new("SOME101");
-        assert!(class.is_ok());
-
-        let class = Class::new("some random text");
-        assert!(class.is_err())
+    fn new_assignment_1() {
+        let assign = Assignment::new("Test 1", 50.0, "SOME101");
+        assert!(assign.is_ok());
+    }
+    #[test]
+    fn new_assignment_2() {
+        let assign = Assignment::new("text is way longer than 20 chars", 101.0, "some text");
+        assert!(assign.is_err());
+    }
+    #[test]
+    fn new_assignment_3() {
+        let assign = Assignment::new("Test 1", 101.0, "some text");
+        assert!(assign.is_err());
+    }
+    #[test]
+    fn new_assignment_4() {
+        let assign = Assignment::new("Test 1", -50.0, "some text");
+        assert!(assign.is_err());
+    }
+    #[test]
+    fn new_assignment_5() {
+        let assign = Assignment::new("Test 1", 50.0, "some text");
+        assert!(assign.is_err());
     }
 
     #[test]
     fn set_mark() {
-        let mut assign = Assignment::new("Test 1", 50.0, Class::new("SOME101").unwrap());
+        let mut assign = Assignment::new("Test 1", 50.0, "SOME101").unwrap();
         assert!(assign.set_mark(80.0).is_ok());
         assert!(assign.set_mark(-80.0).is_err());
         assert!(assign.set_mark(200.0).is_err());
     }
     #[test]
     fn final_pct() {
-        let mut assign = Assignment::new("Test 1", 50.0, Class::new("SOME101").unwrap());
+        let mut assign = Assignment::new("Test 1", 50.0, "SOME101").unwrap();
         assert!(assign.set_mark(100.0).is_ok());
         assert!(!assign.final_pct().is_none());
         assert_eq!(50.0, assign.final_pct().unwrap());
 
         assign.remove_mark();
         assert_eq!(None, assign.final_pct());
+    }
+
+    #[test]
+    fn parse_valid_1() {
+        let a: Result<Assignment, _> = "TEST101,Test 1,75.0,20.0".parse();
+        assert!(a.is_ok());
+        let a = a.unwrap();
+        assert_eq!(
+            Assignment {
+                name: String::from("Test 1"),
+                mark: Some(75.0),
+                value: 20.0,
+                class_code: String::from("TEST101")
+            },
+            a
+        );
+    }
+    #[test]
+    fn parse_valid_2() {
+        let a: Result<Assignment, _> = "TEST101,Test 1,None,20.0".parse();
+        assert!(a.is_ok());
+        let a = a.unwrap();
+        assert_eq!(
+            Assignment {
+                name: String::from("Test 1"),
+                mark: None,
+                value: 20.0,
+                class_code: String::from("TEST101")
+            },
+            a
+        );
+    }
+    #[test]
+    fn parse_invalid_1() {
+        let a: Result<Assignment, _> = ",Test 1,None,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_2() {
+        let a: Result<Assignment, _> = "NOT 101,Test 1,None,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_3() {
+        let a: Result<Assignment, _> = "TEST___,Test 1,None,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_4() {
+        let a: Result<Assignment, _> = "TEST101,,None,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_5() {
+        let a: Result<Assignment, _> =
+            "TEST101,really long assignment name which is invalid,None,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_6() {
+        let a: Result<Assignment, _> = "TEST101,Aa,None,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_7() {
+        let a: Result<Assignment, _> = "TEST101,Test 1,-10.0,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_8() {
+        let a: Result<Assignment, _> = "TEST101,Test 1,110.0,20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_9() {
+        let a: Result<Assignment, _> = "TEST101,Test 1,80.0,-20.0".parse();
+        assert!(a.is_err());
+    }
+    #[test]
+    fn parse_invalid_10() {
+        let a: Result<Assignment, _> = "TEST101,Test 1,80.0,120.0".parse();
+        assert!(a.is_err());
     }
 }
