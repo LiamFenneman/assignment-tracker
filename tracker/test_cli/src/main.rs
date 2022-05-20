841a6a9c81da678bcdf7fae7845d47096273928e
Liam Fenneman <liamfennemanbusiness@gmail.com>
2022-05-20 17:12:23 +1200

    Call more methods to test logs

--- tracker/test_cli/src/main.rs
+++ tracker/test_cli/src/main.rs
@@ -11,12 +11,15 @@ fn main() -> Result<()> {
         .value(50.0)
         .build();
 
-    a.set_mark(100.0)?;
+    a.set_mark(55.0)?;
     a.remove_mark();
     a.remove_mark();
 
-    class.add_assignment(a)?;
-    class.remove_assignment(0)?;
+    let _ = class.add_assignment(a);
+    let _ = class.add_mark(0, 75.0);
+    let _ = class.add_mark(1, 10.0);
+    let _ = class.add_mark(0, -10.0);
+    let _ = class.remove_assignment(0);
 
     Ok(())
 }
