486951a61c2c21110d205ae5bcba4c5d2304a110
Liam Fenneman <liamfennemanbusiness@gmail.com>
2022-05-31 21:25:26 +1200

    Add test cases for `Mark`

--- tracker/core/src/mark.rs
+++ tracker/core/src/mark.rs
@@ -181,31 +181,108 @@ mod tests {
     use super::Mark::{self, Letter, OutOf, Percent};
     use rstest::rstest;
 
-    #[rstest]
-    #[case(Percent(0.0))]
-    #[case(Percent(75.0))]
-    #[case(Percent(100.0))]
-    #[case(Letter('A'))]
-    #[case(Letter('E'))]
-    #[case(Letter('Z'))]
-    #[case(OutOf(0, 0))]
-    #[case(OutOf(22, 25))]
-    #[case(OutOf(90, 100))]
-    #[case(OutOf(1, 1))]
-    fn valid(#[case] mark: Mark) {
-        assert!(mark.check_valid().is_ok());
+    mod check_valid {
+        use super::*;
+
+        #[rstest]
+        #[case(Percent(0.0))]
+        #[case(Percent(0.05))]
+        #[case(Percent(75.0))]
+        #[case(Percent(100.0))]
+        #[case(Letter('A'))]
+        #[case(Letter('E'))]
+        #[case(Letter('Z'))]
+        #[case(OutOf(0, 0))]
+        #[case(OutOf(22, 25))]
+        #[case(OutOf(90, 100))]
+        #[case(OutOf(1, 1))]
+        fn ok(#[case] mark: Mark) {
+            assert!(mark.check_valid().is_ok());
+        }
+
+        #[rstest]
+        #[case(Percent(-0.1))]
+        #[case(Percent(100.1))]
+        #[case(Letter('a'))]
+        #[case(Letter('$'))]
+        #[case(Letter('0'))]
+        #[case(Letter('%'))]
+        #[case(OutOf(1, 0))]
+        #[case(OutOf(10, 5))]
+        fn err(#[case] mark: Mark) {
+            assert!(mark.check_valid().is_err());
+        }
     }
 
-    #[rstest]
-    #[case(Percent(-0.1))]
-    #[case(Percent(100.1))]
-    #[case(Letter('a'))]
-    #[case(Letter('$'))]
-    #[case(Letter('0'))]
-    #[case(Letter('%'))]
-    #[case(OutOf(1, 0))]
-    #[case(OutOf(10, 5))]
-    fn invalid(#[case] mark: Mark) {
-        assert!(mark.check_valid().is_err());
+    mod percent {
+        use super::*;
+
+        #[rstest]
+        #[case(0.0)]
+        #[case(0.05)]
+        #[case(33.333)]
+        #[case(75.0)]
+        #[case(100.0)]
+        fn ok(#[case] pct: f64) {
+            assert!(Mark::percent(pct).is_ok());
+        }
+
+        #[rstest]
+        #[case(-1.0)]
+        #[case(-0.05)]
+        #[case(133.333)]
+        #[case(175.0)]
+        #[case(-100.0)]
+        fn err(#[case] pct: f64) {
+            assert!(Mark::percent(pct).is_err());
+        }
+    }
+
+    mod letter {
+        use super::*;
+
+        #[rstest]
+        #[case('A')]
+        #[case('B')]
+        #[case('D')]
+        #[case('M')]
+        #[case('Z')]
+        fn ok(#[case] c: char) {
+            assert!(Mark::letter(c).is_ok());
+        }
+
+        #[rstest]
+        #[case('a')]
+        #[case('2')]
+        #[case('$')]
+        #[case(' ')]
+        #[case('\n')]
+        fn err(#[case] c: char) {
+            assert!(Mark::letter(c).is_err());
+        }
+    }
+
+    mod out_of {
+        use super::*;
+
+        #[rstest]
+        #[case(u32::MIN, u32::MIN)]
+        #[case(u32::MAX, u32::MAX)]
+        #[case(66, 88)]
+        #[case(20, 24)]
+        #[case(u32::MIN, u32::MAX)]
+        fn ok(#[case] a: u32, #[case] b: u32) {
+            assert!(Mark::out_of(a, b).is_ok());
+        }
+
+        #[rstest]
+        #[case(u32::MAX, u32::MIN)]
+        #[case(u32::MAX, 0)]
+        #[case(10, 9)]
+        #[case(99, 10)]
+        #[case(25, 24)]
+        fn err(#[case] a: u32, #[case] b: u32) {
+            assert!(Mark::out_of(a, b).is_err());
+        }
     }
 }
