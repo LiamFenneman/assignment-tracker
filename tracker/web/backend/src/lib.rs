85142ed3ff09219729966edd31b4ee9a9a989af0
Liam Fenneman <liamfennemanbusiness@gmail.com>
2022-06-05 00:04:48 +1200

    Implement creation of new trackers
    Endpoint is now: `/tracker/new`

--- tracker/web/backend/src/lib.rs
+++ tracker/web/backend/src/lib.rs
@@ -1,11 +1,51 @@
+#![feature(let_else)]
+
 #[macro_use]
 extern crate worker;
 
+use tracker_core::prelude::*;
+use uuid::Uuid;
 use worker::*;
 
 mod utils;
 
 const KV_NAMESPACE: &str = "TRACKER_KV";
+
+async fn generate_new_tracker<D>(req: Request, ctx: RouteContext<D>) -> Result<Response> {
+    // generate a new id that will be the key in the kv store for the tracker
+    // NOTE: this could potentially use v5 using some unique user data (e.g. email, name)
+    let id = Uuid::new_v4();
+
+    // get the optional url query params
+    let url = req.url()?;
+    let name = url.query_pairs().find(|(k, _)| k == "name");
+
+    // create an empty tracker with an optional name
+    // TODO: allow using Code or Class
+    let tracker: Tracker<Code> = match name {
+        Some((_, name)) => Tracker::new(&name),
+        _ => Tracker::default(),
+    };
+
+    // get access to kv store
+    let Ok(kv) = ctx.kv(KV_NAMESPACE) else {
+                return Response::error("Internal Server Error", 500);
+            };
+
+    // put the tracker into the kv store using the uuid created
+    // TODO: replace format! with serialized Tracker
+    if kv
+        .put(&id.to_string(), format!("{:?}", tracker))?
+        .execute()
+        .await
+        .is_ok()
+    {
+        // tracker was successfully put into kv store, return the uuid with status 201
+        return Ok(Response::ok(id.to_string())?.with_status(201));
+    }
+
+    Response::error("Bad Request", 400)
+}
 
 #[event(fetch)]
 pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
@@ -16,6 +56,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
     let router = Router::new();
     router
         .get("/", |_, _| Response::ok("Hello from Workers!"))
+        .post_async("/tracker/new", generate_new_tracker)
         .get("/tracker/:id", |_, ctx| {
             let s = String::new();
             match ctx.param("id").unwrap_or(&s).parse::<u32>() {
@@ -45,10 +86,6 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             };
 
             Response::error("Bad Request", 400)
-        })
-        .get("/worker-version", |_, ctx| {
-            let version = ctx.var("WORKERS_RS_VERSION")?.to_string();
-            Response::ok(version)
         })
         .run(req, env)
         .await
