ee7516d7ef7f1ebe5469708e28889b4c0b4f2abd
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-01-26 22:54:14 +1300

    Basic implementation of a course

--- core/src/course.rs
+++ core/src/course.rs
@@ -1,5 +1,27 @@
-/// Representation of a Course.
+use crate::Assignment;
+
+/// Representation of a [Course].
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Course {
     pub name: String,
+    pub assignments: Vec<Assignment>,
+}
+
+impl Course {
+    /// Create a new [Course] providing a name.
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_owned(),
+            ..Default::default()
+        }
+    }
+}
+
+impl Default for Course {
+    fn default() -> Self {
+        Self {
+            name: String::from("Unknown course"),
+            assignments: Vec::new(),
+        }
+    }
 }
